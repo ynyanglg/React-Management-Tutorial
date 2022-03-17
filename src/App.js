b3cf6ed01d000a4eb2794e605cddafec4f8269a4
DESKTOP-4RCQSHQ\ynyan <ynyang@naonworks.com>
2022-03-17 14:59:31 +0900

    Apply Material UI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,35 @@
 import { Component } from "react";
 import Customer from "./components/Customer";
 import "./App.css";
+import Paper from "@mui/material/Paper";
+import Table from "@mui/material/Table";
+import TableHead from "@mui/material/TableHead";
+import TableBody from "@mui/material/TableBody";
+import TableRow from "@mui/material/TableRow";
+import TableCell from "@mui/material/TableCell";
+import { withStyles } from "@mui/styles";
+
+/*
+const styles = {
+  root: {
+    width: "100%",
+
+    overflowX: "auto",
+  },
+  table: {
+    minWidth: 1080,
+  },
+};
+*/
+const styles = (theme) => ({
+  root: {
+    width: "100%",
+    overflowX: "auto",
+  },
+  table: {
+    minWidth: 1080,
+  },
+});
 
 const customers = [
   {
@@ -31,8 +60,36 @@ const customers = [
 
 class App extends Component {
   render() {
+    const { classes } = this.props;
     return (
-      <div>
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생면월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {customers.map((c) => {
+              return (
+                <Customer
+                  key={c.id}
+                  id={c.id}
+                  image={c.image}
+                  name={c.name}
+                  birthday={c.birthday}
+                  gender={c.gender}
+                  job={c.job}
+                />
+              );
+            })}
+          </TableBody>
+        </Table>
         {customers.map((c) => {
           return (
             <Customer
@@ -46,9 +103,9 @@ class App extends Component {
             />
           );
         })}
-      </div>
+      </Paper>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);

692b1e74a0cdd120abad783621da240df7d6549c
DESKTOP-4RCQSHQ\ynyan <ynyang@naonworks.com>
2022-03-18 15:50:42 +0900

    Create Mysql DB & Node.js Database Customers Set

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require("fs");
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
@@ -6,13 +7,36 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// test
+const json = '{"result":true, "count":42}';
+const obj = JSON.parse(json);
+console.log(obj.count);
+// expected output: 42
+console.log(obj.result);
+// expected output: true
+
+const data = fs.readFileSync("./database.json");
+const conf = JSON.parse(data);
+const mysql = require("mysql");
+const connection = mysql.createConnection({
+  host: conf.host,
+  user: conf.user,
+  password: conf.password,
+  port: conf.port,
+  database: conf.database,
+});
+
+connection.connect();
+
 /*
 app.get("/api/hello", (req, res) => {
   res.send({ messgae: "Hello Express!!" });
 });
 */
 app.get("/api/customers", (req, res) => {
+  /*
   res.send([
+    
     {
       id: 1,
       image: "https://placeimg.com/64/64/1",
@@ -37,7 +61,12 @@ app.get("/api/customers", (req, res) => {
       gender: "여성",
       job: "디자이너",
     },
+    
   ]);
+  */
+  connection.query("SELECT * FROM CUSTOMER", (err, rows, feilds) => {
+    res.send(rows);
+  });
 });
 
 app.listen(port, () =>
